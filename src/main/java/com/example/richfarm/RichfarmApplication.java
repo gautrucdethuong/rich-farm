c54c2e3f95b02116133991c9aeac4f33e71daa53
Huynh Nhat Minh <minhhuynh051120@gmail.com>
2022-10-20 22:06:45 +0700

    add project folder into github

---
+++ src/main/java/com/example/richfarm/RichfarmApplication.java
@@ -0,0 +1,13 @@
+package com.example.richfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichfarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichfarmApplication.class, args);
+	}
+
+}
